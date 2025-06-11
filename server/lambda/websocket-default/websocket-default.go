5a3805bf3070b5e74aeb224aad289fdc65e0c2b7
linyu3294 <yu.lin@later.com>
2025-06-10 22:02:01 -0400

    Send back connectionId once client connects

--- server/lambda/websocket-default/websocket-default.go
+++ server/lambda/websocket-default/websocket-default.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
 )
 
+func sendConnectionIdToClient(ctx context.Context, endpoint, connectionId string) error {
+	sess := session.Must(session.NewSession())
+	api := apigatewaymanagementapi.New(sess, aws.NewConfig().WithEndpoint(endpoint))
+	msg, _ := json.Marshal(map[string]string{"connectionId": connectionId})
+	_, err := api.PostToConnectionWithContext(ctx, &apigatewaymanagementapi.PostToConnectionInput{
+		ConnectionId: aws.String(connectionId),
+		Data:         msg,
+	})
+	return err
+}
+
 func handler(ctx context.Context, req events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// Log the incoming message and connection ID
 	fmt.Printf("Received message: %s\n", req.Body)
 	fmt.Printf("Connection ID: %s\n", req.RequestContext.ConnectionID)
 
-	// You can add logic here to handle different message types, actions, etc.
+	endpoint := req.RequestContext.DomainName
+	if !strings.HasPrefix(endpoint, "https://") && !strings.HasPrefix(endpoint, "http://") {
+		endpoint = "https://" + endpoint
+	}
+	endpoint = endpoint + "/" + req.RequestContext.Stage
+
+	err := sendConnectionIdToClient(ctx, endpoint, req.RequestContext.ConnectionID)
+	if err != nil {
+		fmt.Printf("Failed to send connectionId to client: %v\n", err)
+	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
