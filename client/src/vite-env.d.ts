95301382e85fe7502cc14d3faa036517820d8217
linyu3294 <yu.lin@later.com>
2025-06-12 19:11:38 -0400

    Update vite-env.d.ts with VITE_API_* types and ensure it is not ignored

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,7 +1,15 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly NODE_ENV: string;
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_API_HTTPS_URL: string;
+  readonly VITE_API_WEBSOCKET_URL: string;
+  readonly VITE_API_KEY: string;
+  readonly VITE_API_GATEWAY_URL: string;
+  // Add other environment variables here
+}
+
 interface ImportMeta {
-    env: {
-      NODE_ENV: string;
-      VITE_APP_TITLE: string;
-      // Add other environment variables here
-    };
-  }
+  readonly env: ImportMetaEnv;
+}
